a7546b0918c403fb43dab3fbf8f7f3ed95cb10a8
tylerhaugen-stanley <tyler.haugen.stanley@me.com>
2016-07-12 17:59:36 -0400

    Adding express and postgres support. Updated server.js to use express

--- server.js
+++ server.js
@@ -2,45 +2,69 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
+var express = require('express');
 
-function send404(response) {
-  response.writeHead(404, {"Content-type" : "text/plain"});
-  response.write("Error 404: resource not found");
-  response.end();
-}
+var app = express();
 
-function sendPage(response, filePath, fileContents) {
-  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
-  response.end(fileContents);
-}
+app.use("/public", express.static(__dirname + '/public'));
 
-function serverWorking(response, absPath) {
-  fs.exists(absPath, function(exists) {
-    if (exists) {
-      fs.readFile(absPath, function(err, data) {
-        if (err) {
-          send404(response)
-        } else {
-          sendPage(response, absPath, data);
-        }
-      });
-    } else {
-      send404(response);
-    }
-  });
-}
+app.get('/', function(request, response){
+  response.sendFile('public/index.html', {root: __dirname });
+});
 
-var server = http.createServer(function(request, response) {
-  var filePath = false;
+app.listen(process.env.PORT || 3000);
 
-  if (request.url == '/') {
-    filePath = "public/index.html";
-  } else {
-    filePath = "public" + request.url;
-  }
+// function send404(response) {
+//   response.writeHead(404, {"Content-type" : "text/plain"});
+//   response.write("Error 404: resource not found");
+//   response.end();
+// }
 
-  var absPath = "./" + filePath;
-  serverWorking(response, absPath);
-});	
+// function sendPage(response, filePath, fileContents) {
+//   response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+//   response.end(fileContents);
+// }
 
-var port_number = server.listen(process.env.PORT || 3000);
+// function serverWorking(response, absPath) {
+//   fs.exists(absPath, function(exists) {
+//     if (exists) {
+//       fs.readFile(absPath, function(err, data) {
+//         if (err) {
+//           send404(response)
+//         } else {
+//           sendPage(response, absPath, data);
+//         }
+//       });
+//     } else {
+//       send404(response);
+//     }
+//   });
+// }
+
+// var server = http.createServer(function(request, response) {
+//   var filePath = false;
+
+//   if (request.url == '/') {
+//     filePath = "public/index.html";
+//   } else {
+//     filePath = "public" + request.url;
+//   }
+
+//   var absPath = "./" + filePath;
+//   serverWorking(response, absPath);
+// });	
+
+// var port_number = server.listen(process.env.PORT || 3000);
+
+
+
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
