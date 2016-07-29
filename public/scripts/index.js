d328887af25b089a7c6d56a3951fd4bcb7090898
tylerhaugen-stanley <tyler.haugen.stanley@me.com>
2016-07-29 08:47:38 -0400

    Lots of server side and client side changes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,40 +1,98 @@
+var dbQueryData =[];
+var numImagesToDisplay = 33;
+const imageIncrement = 33;
+
 var CollectionViewer = React.createClass({
+  getInitialState: function(){
+    return{collectionData: []};
+  },
+  updateCollectionToDisplay: function(numImagesToDisplay){
+    this.setState({collectionData: dbQueryData.slice(0, numImagesToDisplay)});
+  },
   handleCollectionQuerySubmit: function(queryData){
-    console.log(queryData);
+    //console.log(queryData);
+    this.setState({collectionData: []});
+    $('#loadMore').hide();
+    $('#emptyQuery').hide();
+    if (queryData.queryType == "createCollection"){
+      $.ajax({
+        type: "POST",
+        url: "createCollection",
+        data: queryData,
+        success: function(data) {
+          console.log(JSON.parse(data));
+          $('#loadingMessage').hide();
+          this.setState({collectionData: JSON.parse(data).data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+        }.bind(this)
+      });
+    } else { // Must be a query on an existing collection
+      numImagesToDisplay = imageIncrement; // New query, reset num images to show.
+      $.ajax({
+        type: "GET",
+        url: "queryCollection",
+        data: queryData,
+        success: function(data) {
+          console.log(data);
+          $('#loadingMessage').hide();
+          if (data.length != 0){
+            dbQueryData = data;
+            $('#loadMore').show();
+            this.updateCollectionToDisplay(numImagesToDisplay);
+          } else {
+            $('#emptyQuery').show();
+          }
+        }.bind(this),
+        error: function(xhr, status, err) {
+        }.bind(this)
+      });
+    }
   },
   render: function(){
     return (
       <div className="collectionViewer">
         <CollectionQuery handleCollectionQuerySubmit={this.handleCollectionQuerySubmit} />
-        <Collection />
+        <Collection collectionData={this.state.collectionData} updateCollectionToDisplay={this.updateCollectionToDisplay}/>
       </div>
     );
   }
 });
+
 var CollectionQuery = React.createClass({
   getInitialState: function(){
-    return{queryType: 'createCollection', tag: '', startDate: '', endDate: ''};
+    var curDate = new Date().toISOString().slice(0,10); // Set start and end date to the current date
+    return{queryType: 'createCollection', hashtag: '', startDate: curDate, endDate: curDate};
   },
 	handleSubmit: function(event){
     event.preventDefault();
     var queryType = this.state.queryType;
-    var tag = this.state.tag.trim();
+    var hashtag = this.state.hashtag.trim();
     var startDate = this.state.startDate.trim();
     var endDate = this.state.endDate.trim();
 
-    // ensure all fields are filled out.
-    if (!tag || !startDate || !endDate){
+    // Ensure all fields are filled out.
+    if (!hashtag){
+      // TODO: Explain error.
+
+      return;
+    }
+    if (startDate > endDate){
+      // TODO: Explain error
       return;
     }
 
+    // Show a loading message while the query executes
+    $('#loadingMessage').show();
+
     // Handle the actual query in the 'CollectionViewer'
-    this.props.handleCollectionQuerySubmit({queryType: queryType, tag: tag, startDate: startDate, endDate: endDate});
+    this.props.handleCollectionQuerySubmit({queryType: queryType, hashtag: hashtag, startDate: startDate, endDate: endDate});
   },
   handleRadioButtonChange: function(event){
     this.setState({queryType: event.target.id});
   },
-  handleTagChange: function(event){
-    this.setState({tag: event.target.value});
+  handlehashtagChange: function(event){
+    this.setState({hashtag: event.target.value});
   },
   handleStartDateChange: function(event){
     this.setState({startDate: event.target.value});
@@ -44,26 +102,96 @@ var CollectionQuery = React.createClass({
   },
   render: function(){
 		return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="radio" id="createCollection" name="queryType" onChange={this.handleRadioButtonChange} defaultChecked /> Create Collection
-        <input type="radio" id="queryCollection" name="queryType" onChange={this.handleRadioButtonChange} /> Query Collection
-        Instagram Tag: <input type="text" placeholder="#" value={this.state.tag} onChange={this.handleTagChange} />
-        Start Date: <input type="date" name="startDate" value={this.state.startDate} onChange={this.handleStartDateChange} />
-        End Date: <input type="date" name="endDate" value={this.state.endDate} onChange={this.handleEndDateChange} />
-        Submit: <input type="submit" />
-      </form>
+      <div id="queryDiv">
+        <form onSubmit={this.handleSubmit}>
+          <span id="querySelection">
+            <input type="radio" id="createCollection" name="queryType" onChange={this.handleRadioButtonChange} defaultChecked/> Create Collection          
+          </span>
+          <span id="">
+            <input type="radio" id="queryCollection" name="queryType" onChange={this.handleRadioButtonChange} /> Query Collection
+          </span>
+          <span id="hashtag">
+            Instagram Hashtag: <input type="text" placeholder="#puppy" value={this.state.hashtag} onChange={this.handlehashtagChange} required/>
+          </span>
+          <span id="startDate">
+            Start Date: <input type="date" name="startDate" value={this.state.startDate} onChange={this.handleStartDateChange} />
+          </span>
+          <span id="endDate">
+            End Date: <input type="date" name="endDate" value={this.state.endDate} onChange={this.handleEndDateChange} />
+          </span>
+          <br/> <input id="submitButton" type="submit" />
+        </form>
+        <p id='loadingMessage' hidden>Loading...</p>
+        <p id='emptyQuery' hidden>No Collection with that Hashtag, check date range.</p>
+      </div>
     );
 	}
 });
 
 var Collection = React.createClass({
+  loadMore: function(){
+    numImagesToDisplay += imageIncrement;
+    if (numImagesToDisplay > dbQueryData.length){
+      $('#loadMore').hide();
+    }
+    this.props.updateCollectionToDisplay(numImagesToDisplay);
+  },
   render: function(){
+    var instagramNodes = this.props.collectionData.map(function(instagramPost) {
+      if (instagramPost.tag_time){ // Collection from DB, parse using my JSON structure.
+        return (
+          <Media author={instagramPost.username} media_type={instagramPost.media_type} media={instagramPost.media_url} original_link={instagramPost.original_url} key={instagramPost.id}/>
+        )
+      } else { // New collection, parse using instagram JSON structure
+        return (
+          <Media author={instagramPost.user.username} media_type={instagramPost.type} media={instagramPost.type === "image" ? instagramPost.images.standard_resolution.url : instagramPost.videos.standard_resolution.url} original_link={instagramPost.link} key={instagramPost.id}/>
+        );
+      }
+    });
+
     return(
-      <p> I Am A Collection </p>
+      <div>
+        {instagramNodes}
+        <br />
+        <button id='loadMore' hidden='true' onClick={this.loadMore} name="Load More">Load more</button>
+      </div>
     );
   }
 });
 
+var Media = React.createClass({
+  render: function(){
+    var header = (
+      <header id='mediaHeader'>
+          <a id="author" target="_blank" href={"https://instagram.com/".concat(this.props.author)}>{this.props.author}</a>
+          <a id="instagramLink" target="_blank" href={this.props.original_link}>Instagram link</a> <br />
+      </header>
+    );
+
+    if (this.props.media_type === "image"){
+      return (
+        <div>
+          {header}
+          <div id="mediaDiv">
+            <img id="media" height="640px" width="640px" src={this.props.media} /> <br />
+          </div>
+        </div>
+      );
+    } else { // Need to display a video
+      console.log()
+      return (
+        <div>
+          {header}
+          <div id="mediaDiv">
+            <video id="media" height="640px" width="640px" controls> 
+              <source src={this.props.media} type="video/mp4"/> 
+            </video> <br />
+          </div>
+        </div>
+      );
+    }
+  }
+});
 ReactDOM.render(
   <CollectionViewer />,
   document.getElementById('content')
--- server.js
+++ server.js
@@ -3,8 +3,23 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var express = require('express');
+var url = require('url');
+var request = require('request');
+var pg = require('pg');
 
+const unixTimestampDivisor = 1000; // Used to convert time in miliseconds to a unix timestamp.
+const unixTimestampSecondsInDay = 86399;
 var app = express();
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+const access_token = "272855367.b6f7db4.27aee70b486a4fd7b1b5546c1da0453d";
+const db_url = "postgres://fuaympypihyvwa:GUKk5aDGLgvOkab9XmyVYimW52@ec2-54-235-90-96.compute-1.amazonaws.com:5432/d19ldptt355nm9";
+pg.defaults.ssl = true;
 
 app.use("/public", express.static(__dirname + '/public'));
 
@@ -12,59 +27,202 @@ app.get('/', function(request, response){
   response.sendFile('public/index.html', {root: __dirname });
 });
 
+// Create a collection in the database.
+// Return the collection of photos??
+app.post('/createCollection', function(request, response){
+
+  // Remove leading '#' if one exists.
+  var hashtag = request.body.hashtag.charAt(0) === '#' ? request.body.hashtag.substring(1) : request.body.hashtag;
+  var startDateUnixTimestamp = convertToUnixTimestamp(request.body.startDate);
+  var endDateUnixTimestamp = convertToUnixTimestamp(request.body.endDate) + unixTimestampSecondsInDay; // Set to end of day instead of beginning.
+ 
+  var options = {
+    "count": 33,
+    "callback": parseMediaForClosestEndDate,
+    "endDate": endDateUnixTimestamp,
+    "startDate": startDateUnixTimestamp
+  };
+
+  // Get and ID of an instagram photo from right now.
+  queryInstagram(hashtag, options, response);
+  //queryInstagram(hashtag, {"count": 5, "minTagId": "AQCZFOx6aTUz24hx8SnHwt075inSmYXxmyCSm8c4sJo5HYulejtKUhOPN2e7o_kHXd8Cqqc94INlAlTOh0optSdyiKVXb9HnyqEV7mtWR_tEHULhMyp4nvML31Xsxaz9rNk"}, response);
+  //queryInstagram(hashtag, {"count": 50}, setMediaClosestToEndDate, endDateUnixTimestamp);
+  
+});
+
+// Get a collection of photos if one exists. 
+app.get('/queryCollection', function(request, response){
+  var url_parts = url.parse(request.url, true);
+  var query = url_parts.query;
+  // Remove leading '#' if one exists.
+  var hashtag = query.hashtag.charAt(0) === '#' ? query.hashtag.substring(1) : query.hashtag;
+  var startDateUnixTimestamp = convertToUnixTimestamp(query.startDate);
+  var endDateUnixTimestamp = convertToUnixTimestamp(query.endDate) + unixTimestampSecondsInDay; // Set to end of day instead of beginning.
+
+  console.log("Querying database");
+  queryDatabase(hashtag, startDateUnixTimestamp, endDateUnixTimestamp, response);
+  //{"count": url_parts.query.count}
+  //queryInstagram(hashtag, response);
+});
+
 app.listen(process.env.PORT || 3000);
 
-// function send404(response) {
-//   response.writeHead(404, {"Content-type" : "text/plain"});
-//   response.write("Error 404: resource not found");
-//   response.end();
-// }
 
-// function sendPage(response, filePath, fileContents) {
-//   response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
-//   response.end(fileContents);
-// }
+// TODO: REMOVE
+// Create table query
+// client.query("CREATE TABLE COLLECTIONS(ID SERIAL PRIMARY KEY NOT NULL, tag_time INT NOT NULL, hashtag TEXT NOT NULL, username TEXT NOT NULL, media_type TEXT NOT NULL, media_url TEXT NOT NULL, original_url TEXT NOT NULL);");
 
-// function serverWorking(response, absPath) {
-//   fs.exists(absPath, function(exists) {
-//     if (exists) {
-//       fs.readFile(absPath, function(err, data) {
-//         if (err) {
-//           send404(response)
-//         } else {
-//           sendPage(response, absPath, data);
-//         }
-//       });
-//     } else {
-//       send404(response);
-//     }
-//   });
-// }
+/*
+* Functions
+*/
 
-// var server = http.createServer(function(request, response) {
-//   var filePath = false;
+function parseMediaForClosestEndDate(instagramData, endDate, startDate, hashtag, minTagId, parentResponse){
+  var instagramResponse = JSON.parse(instagramData);
+  var instagramMedia = instagramResponse.data;
+  var index;
+  var allPhotosWithinDateRange = true;
+  
+  var options = {
+    "count": 33,
+    "maxTagId": instagramResponse.pagination.next_max_id,
+    "minTagId": minTagId,
+    "callback": parseMediaForClosestEndDate,
+    "endDate": endDate,
+    "startDate": startDate
+  };
 
-//   if (request.url == '/') {
-//     filePath = "public/index.html";
-//   } else {
-//     filePath = "public" + request.url;
-//   }
+  for (index = 0; index < instagramMedia.length; index ++){
+    var currentMediaCreatedTime = instagramMedia[index].created_time;
 
-//   var absPath = "./" + filePath;
-//   serverWorking(response, absPath);
-// });	
+    // Save the oldest media
+    console.log("Comparing curMedia time: " + convertToDate(currentMediaCreatedTime) + " to endDate: " + convertToDate(endDate));
+    if (currentMediaCreatedTime > endDate){
+      // If a photo is newer than the endDate, need to run the query again.
+      allPhotosWithinDateRange = false;
+      console.log("ONE DATE IS BAD");
+      break;
+    }
+    if (currentMediaCreatedTime < startDate){
+      console.log("*********** Photo is older than start date");
+      options.minTagId = instagramResponse.pagination.min_tag_id;
+      allPhotosWithinDateRange = false;
+    }
+  }
 
-// var port_number = server.listen(process.env.PORT || 3000);
+  if (!allPhotosWithinDateRange){ // If no media found yet, repeat the instagram query.
+    console.log("calling query instagram again");
 
+    queryInstagram(hashtag, options, parentResponse);
+  } else {
+    console.log("Found an appropriate date, sending media back");
+    //options.callback = printMedia;
+    parentResponse.send(instagramData);
+    //saveCollection(instagramMedia, hashtag);
+    saveCollectionInDB(instagramMedia, hashtag);
+  }
+}
 
+function queryDatabase(hashtag, startDateUnixTimestamp, endDateUnixTimestamp, parentResponse){
+  pg.connect(process.env.DATABASE_URL || db_url, function(err, client){
+    if (err) throw err;
+    var results = [];
 
-// var express = require('express');
-// var app = express();
+    var query = client.query('SELECT * FROM COLLECTIONS WHERE tag_time BETWEEN $1 AND $2 AND hashtag = $3;', [startDateUnixTimestamp, endDateUnixTimestamp, hashtag], function(err, result){
+      if (err) throw err;
+    });
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+    query.on('row', function(row){
+      console.log("pushing results");
+      results.push(row);
+    });
 
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
+    query.on('end', function(){
+      console.log("Sending DB data back");
+      // console.log(results);
+      parentResponse.send(results);
+    })
+  });
+}
+
+function saveCollectionInDB(instagramMedia, hashtag){
+  var index;
+
+  pg.connect(process.env.DATABASE_URL || db_url, function(err, client) {
+    if (err) throw err;
+
+    for (index = 0; index < instagramMedia.length; index++){
+      var tag_time = instagramMedia[index].created_time;
+      var media_type = instagramMedia[index].type;
+
+      var media_url = media_type === "image" ? instagramMedia[index].images.standard_resolution.url : instagramMedia[index].videos.standard_resolution.url; 
+      console.log(media_type + ": " + media_url);
+
+      if (media_type === "image"){
+        console.log("image");
+      } else {console.log("video");}
+      // var media_url = instagramMedia[index].images.standard_resolution.url;
+      var original_url = instagramMedia[index].link;
+      var username = instagramMedia[index].user.username;
+
+      client.query("INSERT INTO COLLECTIONS(tag_time, hashtag, username, media_type, media_url, original_url) values($1, $2, $3, $4, $5, $6)", [tag_time, hashtag, username, media_type, media_url, original_url]);
+    }
+  });
+}
+
+function printMedia(instagramData){
+  var instagramMedia = JSON.parse(instagramData).data;
+  var index;
+
+  for (index = 0; index < instagramMedia.length; index++){
+    console.log(convertToDate(instagramMedia[index].created_time));
+  }
+}
+
+function queryInstagram(hashtag, options, parentResponse){
+  var url = "https://api.instagram.com/v1/tags/" + hashtag + "/media/recent?access_token=" + access_token;
+
+  // Configure the URL to adhere to the optional paramaters. 
+  if (options.count){
+     url += "&count=" + options.count;
+  }
+  if (options.minTagId){
+    url += "&min_tag_id=" + options.minTagId;
+  }
+  if (options.maxTagId){
+    url += "&max_tag_id=" + options.maxTagId;
+  }
+
+  console.log("About to send query, url is ... ");
+  console.log(url);
+
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log("Successful instagram query");
+      
+      switch(options.callback){
+        case parseMediaForClosestEndDate:
+          // options.callback(body, options.endDate, hashtag, parentResponse);
+          parseMediaForClosestEndDate(body, options.endDate, options.startDate, hashtag, options.minTagId, parentResponse);
+          break;
+        default:
+          console.log("IN DEFAULT");
+          console.log(options.callback);
+          break;
+      }
+    } else {
+      console.log(error);
+      console.log(response.body);
+    }
+  });
+}
+
+// Take a date in string format, and return a numerical value representing that date as a unix timestamp
+function convertToUnixTimestamp(date){
+  return new Date(date).getTime() / unixTimestampDivisor;
+}
+
+function convertToDate(unixTimestamp){
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  var date = new Date(unixTimestamp*1000);
+  return date.toUTCString();;
+}
